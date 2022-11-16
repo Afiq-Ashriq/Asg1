89f85712e929d225727b875d517486edc63bd437
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2022-11-16 11:28:15 +0800

    Testing javascript scrolling animation

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#firstline", {
-    x: -10000,
-    delay: 0.2,
-    duration: 2,
-    yoyo: true,
-    scrollTrigger: "#firstline"
-})
-
-gsap.from(".container", {
-    x: 10000,
-    delay: 0.2,
-    duration: 2,
-    yoyo: true,
-    scrollTrigger: ".container"
-})
 
 
-gsap.from(".albums", {
-    y: -200,
-    delay: 0.2,
-    duration: 2,
-    yoyo: true,
-    scrollTrigger: ".albums"
-})
+// gsap.from(".container", {
+//     x: 10000,
+//     duration: 0.5,
+//     ease: "power1.inOut",
+//     scrollTrigger: ".container"
+// })
+
+
 
 
 gsap.from(".gallery > img", 1, {
     scale: 0.1, 
     y: 100,
     ease: "power1.inOut",
-    delay: 1,
+    delay: .2,
     stagger: {
-      amount: 1.5, 
+      amount: .5, 
       grid: "auto",
       from: "end"
     },
