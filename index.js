a888c37ff201fa853b9b17d1b84a47ac777139f1
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2022-12-01 22:34:55 +0800

    Added footer to all pages Added filter by country for shows.html

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-
-// gsap.from(".container", {
-//     x: 10000,
-//     duration: 0.5,
-//     ease: "power1.inOut",
-//     scrollTrigger: ".container"
-// })
-
-
-
-
 gsap.from(".gallery > img", 1, {
     scale: 0.1, 
     y: 100,
@@ -25,36 +14,15 @@ gsap.from(".gallery > img", 1, {
     scrollTrigger: ".gallery"
   });
 
-
-
   const dc = document.getElementById('display');
+
 
   function thankYou() {
       document.getElementById('message').style.display = 'block'
       document.getElementById('useremail').value = ""
   }
 
-  const phoneview = window.matchMedia('(max-width: 600px)')
-  function Tshirts() {
-    var type = document.querySelectorAll('.Merch')
-    type[1].style.display = 'none'
-    if (phoneview.matches) {
-      type[0].style.display = 'flex'
-    }
-    else {
-      type[0].style.display = 'grid'
-    }
-  }
-  function sweaters() {
-    var type = document.querySelectorAll('.Merch')
-    type[0].style.display = 'none'
-    if (phoneview.matches) {
-      type[1].style.display = 'flex'
-    }
-    else {
-      type[1].style.display = 'grid'
-    }
-  }
+
 
 
 
