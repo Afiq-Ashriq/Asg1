907ef123434a4921c194cc77b995db16dcf49864
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2022-12-04 02:25:55 +0800

    Testing quiz feature for website (Added quiz.html)

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
 
-
-gsap.from(".gallery > img", 1, {
-    scale: 0.1, 
-    y: 100,
-    ease: "power1.inOut",
-    delay: .2,
-    stagger: {
-      amount: .5, 
-      grid: "auto",
-      from: "end"
-    },
-    scrollTrigger: ".gallery"
-  });
 
   const dc = document.getElementById('display');
 
